e95e00716ef20399854ed3a8e5104dfa9fab9e16
echatela <edchatelain9@gmail.com>
2026-05-22 12:34:27 +0200

    laptop waybar update and monitor

--- my-omarchy/.config/hypr/monitors.lua
+++ my-omarchy/.config/hypr/monitors.lua
@@ -1,13 +1,6 @@
 hl.monitor({
-	output = "DP-3",
-	mode = "2560x1440@240",
+	output = "eDP-1",
+	mode = "1920x1080",
 	position = "0x0",
 	scale = 1.25
-})
-
-hl.monitor({
-	output = "HDMI-A-1",
-	mode = "1920x1080",
-	position = "auto-center-up",
-	scale = 1
 })
